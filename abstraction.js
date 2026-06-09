5cdf13485da41c07d97e8ff4539e7ba33a5561e3
Moideen Mashad <moideenmashad@gmail.com>
2026-06-09 11:30:46 +0400

    Updated

---
+++ abstraction.js
@@ -0,0 +1,15 @@
+/* Abstraction is an OOP concept that focuses on hiding
+the internal details of an object and exposing only 
+the necessary features to the user. 
+It allows us to create complex systems by breaking them down into 
+simpler, more manageable parts. In JavaScript, we can achieve 
+abstraction using classes and objects. */
+
+// Abstraction example
+class car {
+    constructor(name, series, color) {
+        this.name = name;
+        this.series = series;
+        this.color = color;
+    }
+}
