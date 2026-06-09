0f8f593591b46961b789af0fa4d4a22cb707b2e2
Moideen Mashad <moideenmashad@gmail.com>
2026-06-09 10:40:04 +0400

    Updated

--- class.js
+++ class.js
@@ -10,5 +10,25 @@ class car {
   }
 }
 
+
+class scooter extends car {
+  constructor(name, series, color, type) {
+    super(name, series, color);
+    this.type = type;
+  }
+  getDetails() {
+    return `The scooter is ${this.name} ${this.series} and its color is ${this.color} and its type is ${this.type}`;
+  }
+}
+
+let newScooter = new scooter("Vespa", "LX", "red", "scooter");
+console.log(newScooter.getDetails());
+
+
+
+
 let newCar = new car("BMW", "M3", "black");
 console.log(newCar.getDetails());
+
+console.log(typeof newScooter); // object
+console.log(typeof newCar); // function
