5f412d570781840f8d1a52f3f3dd34d324c5a503
Moideen Mashad <moideenmashad@gmail.com>
2026-06-09 11:35:52 +0400

    Updated

--- abstraction.js
+++ abstraction.js
@@ -6,10 +6,32 @@ simpler, more manageable parts. In JavaScript, we can achieve
 abstraction using classes and objects. */
 
 // Abstraction example
-class car {
+
+class Car {
     constructor(name, series, color) {
         this.name = name;
         this.series = series;
         this.color = color;
     }
+
+    // Public method
+    getCarDetails() {
+        return `${this.name} ${this.series} (${this.color})`;
+    }
+
+    // Internal method (users don't need to know how it works)
+    startEngine() {
+        return "Engine Started...";
+    }
+
+    // Exposed method
+    drive() {
+        return `${this.startEngine()} Car is moving.`;
+    }
 }
+
+// Creating object
+const car1 = new Car("Mercedes", "W15", "Silver");
+
+console.log(car1.getCarDetails());
+console.log(car1.drive());
